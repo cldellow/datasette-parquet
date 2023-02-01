60b5a3eed0250079337ff3c095036bfce0ae4fed
Colin Dellow <cldellow@gmail.com>
2023-01-31 21:45:26 -0500

    refactor

--- datasette_parquet/ddl.py
+++ datasette_parquet/ddl.py
@@ -10,6 +10,7 @@ def sniff_json_columns(fname):
         return ', '.join(["json->>'{}' AS \"{}\"".format(x, x) for x in obj.keys()])
 
 def view_for(view_name, fname, glob):
+    view_name = view_name.replace('.', '_')
     if fname.endswith('.csv') or fname.endswith('.tsv'):
         return "CREATE VIEW \"{}\" AS SELECT * FROM read_csv_auto('{}', header=true)".format(view_name, glob)
     elif fname.endswith('.parquet'):
@@ -36,16 +37,6 @@ def create_views(dirname):
             file = files[0]
             rv.append(view_for(Path(fname).stem, file.path, os.path.join(fname, '*' + Path(file).suffix)))
         else:
-            if fname.endswith('.csv') or fname.endswith('.tsv'):
-                view = Path(fname).stem
-                rv.append("CREATE VIEW \"{}\" AS SELECT * FROM read_csv_auto('{}', header=true)".format(view, fname))
-            elif fname.endswith('.parquet'):
-                view = Path(fname).stem
-                rv.append("CREATE VIEW \"{}\" AS SELECT * FROM '{}'".format(view, fname))
-            elif fname.endswith('.ndjson') or fname.endswith('.jsonl'):
-                view = Path(fname).stem
-                # We need to sniff the first row of the file in order to build a good view
-                columns = sniff_json_columns(fname)
-                rv.append("CREATE VIEW \"{}\" AS SELECT {} FROM read_json_objects('{}')".format(view, columns, fname))
+            rv.append(view_for(Path(fname).stem, fname, fname))
 
-    return rv
+    return [x for x in rv if x]
