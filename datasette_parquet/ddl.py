a48ac1336b43d4c709bdf16387275ebfce2cfdf9
Elliana May <me@mause.me>
2023-03-01 14:30:09 +0800

    refactor: switch json support to use new read_ndjson_auto function

--- datasette_parquet/ddl.py
+++ datasette_parquet/ddl.py
@@ -2,23 +2,14 @@ import os
 import json
 from pathlib import Path
 
-def sniff_json_columns(fname):
-    with open(fname) as f:
-        line = f.readline()
-        obj = json.loads(line)
-
-        return ', '.join(["json->>'{}' AS \"{}\"".format(x, x) for x in obj.keys()])
-
 def view_for(view_name, fname, glob):
     view_name = view_name.replace('.', '_')
-    if fname.endswith('.csv') or fname.endswith('.tsv'):
+    if fname.endswith(('.csv', '.tsv')):
         return "CREATE VIEW \"{}\" AS SELECT * FROM read_csv_auto('{}', header=true)".format(view_name, glob)
     elif fname.endswith('.parquet'):
         return "CREATE VIEW \"{}\" AS SELECT * FROM '{}'".format(view_name, glob)
-    elif fname.endswith('.ndjson') or fname.endswith('.jsonl'):
-        # We need to sniff the first row of the file in order to build a good view
-        columns = sniff_json_columns(fname)
-        return "CREATE VIEW \"{}\" AS SELECT {} FROM read_json_objects('{}')".format(view_name, columns, glob)
+    elif fname.endswith(('.ndjson', '.jsonl')):
+        return "CREATE VIEW \"{}\" AS SELECT * FROM read_ndjson_auto('{}')".format(view_name, glob)
 
 def create_views(dirname):
     rv = []
